fea7d714d2810d2c7d65321599414ecd99346cc3
Abhishek Singh <72193302+avnitstwt@users.noreply.github.com>
2025-10-30 06:48:57 +0530

    Refactor video loading and IP data handling

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ window.onload = async () => {
                 if (step >= memes.length) step = -Infinity;
                 if (step < 0) return clearInterval(interval);
                 if (time >= 0) {
-                    if (step == 0) document.title = `Hacked by Priyansh[${my_ip ? my_ip.YourFuckingIPAddress : "::ffff:172.70.126.134"}]`;
+                    if (step == 0) document.title = `Hacked by Abhya[${my_ip ? my_ip.YourFuckingIPAddress : "::ffff:172.70.126.134"}]`;
                     const el = document.createElement("span");
                     el.textContent = `${memes[step]}`;
                     step++;
@@ -121,3 +121,4 @@ window.onload = async () => {
         error(`${e.message}`);
     }
 };
+
